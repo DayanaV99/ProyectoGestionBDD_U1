6340915f3b398ee0a0742ae7722fb6a018bb44e6
StevenFloril25 <jsfloril@espe.edu.ec>
2024-02-09 08:35:03 -0500

    Actualizaci├│n del respaldo de la base de datos.

--- RespaldoProyecto.sql
+++ RespaldoProyecto.sql
@@ -264,4 +264,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-09 13:26:43
+-- Dump completed on 2024-02-09 13:35:03
